73c39dd2d5f240e8c2a1e43c9e7d62386234a686
Yang Zhengqing <1370483456@qq.com>
2020-11-08 17:26:33 +0800

    create p3s2 and resolve prblem with @Autowired

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -1,5 +1,6 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,6 +9,7 @@ public class GreetingService {
     private final Foo foo;
     private final Bar bar;
 
+    @Autowired
     public GreetingService(Foo foo, Bar bar) {
         this.foo = foo;
         this.bar = bar;
